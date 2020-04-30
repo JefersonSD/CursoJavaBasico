b49e833dbf13d5fe984d7b526c316f12835391b9
Jeferson Silva <jeferson.smarques@sempreceub.com>
2020-04-30 17:01:47 -0300

    Aulas 6 a 11 (Curso java básico Loiane)

---
+++ curso-java-basico/src/cursojava/aula10/Variaveis.java
@@ -0,0 +1,26 @@
+package cursojava.aula10;
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+	// Convenção java
+		int idade = 20;
+		String nome = "Jeferson";
+		String nomeDoMeuCachorro = "totó";
+		String ano2014 = "2014";
+		
+	// Aceito, porém não utilizado
+		int _idade;
+		int $idade;
+		
+	// Não é convensão java
+		
+		int a = 20;
+		String b = "Jeferson";
+		
+		idade = 25;
+		
+		System.out.println("Idade = " + idade);
+	}
+
+}
